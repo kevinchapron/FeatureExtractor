bae23dca715e2b179e391673e851e7b33e55d05b
Kévin <kevin.chapron@live.fr>
2016-11-24 14:12:59 -0500

    Add of real-time data analyzer

--- cmd/app.go
+++ cmd/app.go
@@ -3,6 +3,7 @@ package main
 import "github.com/kevinchapron/FeatureExtractor/examples"
 
 func main() {
-	examples.Multifile()
+	examples.Array()
+	//examples.Multifile()
 	//examples.Singlefile()
 }
